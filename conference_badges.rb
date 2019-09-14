2519da88f3f50dd02512198ffaa3b4efe3cc2ede
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-14 16:32:01 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,11 @@ def batch_badge_creator(name_arr)
     "Hello, my name is #{name}."
   end
 end
+
+def assign_rooms(speaker_arr)
+  speakers = []
+  name_arr.each_with_index do |name, index|
+    speakers_arr << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  speakers_arr
+end
