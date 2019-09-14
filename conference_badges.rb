52a997ed0f6df39d09a9ca4c50d4f17ab5ee5990
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-09-14 19:15:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(conf_speakers)
+  name_badge = []
+  conf_speakers.each do |speaker|
+    name_badge << badge_maker(speaker)
+  end
+  name_badge
+end 
+
+def assign_rooms(conf_speakers)
+  room_assignments = []
+  conf_speakers.each.with_index do |speaker, room_number|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room_number + 1}!"
+  end
+  room_assignments
+end
+
+def printer(array)
+  room = 0
+   batch_badge_creator(array).each do |speaker|
+    puts speaker
+    puts room_assignments[room]
+    room +=1
+    end
+end
