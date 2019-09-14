a926dc8d695980813a255f904fe76ed206fdf10b
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-14 16:15:31 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+  name_arr.collect do |name|
+    "Hello, my name is #{name}."
+  end
+end
