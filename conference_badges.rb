2f5c11973c79a2943adc3311ceab3504fd765e42
Ally Morson <ally.morson@gmail.com>
2019-09-25 02:39:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  names.each do |speaker|
+  puts "Hi my name is #{speaker}"
+  counter = 1 
+  counter += 1 
+end
+end 
